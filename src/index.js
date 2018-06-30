20dac091ef3a877c01fedd3a0b85bd55b04da4e2
Ankeet <ankeet@rei.local>
2018-06-29 18:48:34 -0700

    does this work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+// import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
 
-import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+Vue.config.productionTip = false 
+import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/black-green-light.css'
 
@@ -9,10 +11,15 @@ Vue.use(VueMaterial)
 Vue.use(MdButton)
 Vue.use(MdContent)
 Vue.use(MdTabs)
-  new Vue({
+var app = new Vue({
     el: '#app',
     components: { App },
     template: '<App/>'
 })
 
-alert(4);
+var app22 = new Vue({
+    el: '#app22',
+    data: {
+        message: 'hello vue!'
+    }
+})
---
+++ tmp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <md-toolbar class="md-primary">
+        <h1 class="md-title">Collated Paper Services</h1>
+    </md-toolbar>
+    
+    <md-content>
+        This website is under construction. We are interfacing with your Earth Internet to create an interactive on-line experience. However, your quaint Javascript frameworks are a nightmare to figure out.
+    </md-content>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang="scss">
+.md-content {
+  padding: 16px;
+}
+</style>
